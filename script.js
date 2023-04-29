8e770a436b588d7a57006aab96a0f44f334d21fe
K Anamithra <anamithrak@gmail.com>
2023-04-29 18:43:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let songs = [{
+    name: 'Maan Meri Jaan',
+    path: 'maan meri jaan.mp3',
+    artist: 'King',
+    cover: 'maan.jpg'
+},
+{
+    name: 'Raatan Lambiyan',
+    path: 'Raatan Lambiyan.mp3',
+    artist: 'Jubin Nautiyal, Asees Kaur',
+    cover: 'raatan.jpg'
+},
+{
+    name: 'Until I Found You',
+    path: 'Until-I-Found-You.mp3',
+    artist: 'Stephen Sanchez',
+    cover: 'until.jpg'
+},
+{
+    name: 'Vaaste',
+    path: 'vaaste.mp3',
+    artist: 'Dhvani Bhanushali',
+    cover: 'vaaste.jpg'
+}]
